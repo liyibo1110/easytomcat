d8e7ed5d432bc6d1e3c8492963d4bea645ea1112
liyibo1110 <liyibo1110@sina.com>
2018-09-20 23:52:38 +0800

    增加代码

---
+++ src/main/java/ex03/pyrmont/connector/http/Constants.java
@@ -0,0 +1,9 @@
+package ex03.pyrmont.connector.http;
+
+import java.io.File;
+
+public class Constants {
+
+	//静态文件存储目录
+	public static final String WEB_ROOT = System.getProperty("user.dir") + File.separator + "webroot";
+}
